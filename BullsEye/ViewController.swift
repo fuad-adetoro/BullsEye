5fe788c0af409c8ddb94fd43c3fdad5228d7c7d4
Frederico <BusyFredo@Fuads-MacBook-Pro.local>
2017-01-16 13:14:30 +0000

    Added in a slider, couple labels and connected the actions to the viewController.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  BullsEye
+//  Bulls''Eye
 //
 //  Created by Frederico on 16/01/2017.
 //  Copyright © 2017 Fuad Adetoro. All rights reserved.
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,6 +20,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func showAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        print("The value of the slider is now: \(sender.value)")
+    }
 }
 
