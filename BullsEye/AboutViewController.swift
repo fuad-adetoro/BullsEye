9c91dbce9f35a02ac0ff64c8c1adae63bca8574c
Frederico <BusyFredo@Fuads-MacBook-Pro.local>
2017-01-18 15:01:24 +0000

    Finished creating the bull's eye game without any design.
    When the (i) is clicked a segue to the aboutViewController happens, in the aboutViewController close dismisses itself.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Frederico on 18/01/2017.
+//  Copyright © 2017 Fuad Adetoro. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 0
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scoreLabel.text = "0"
+        roundLabel.text = "1"
+        startNewGame()
+        updateLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,14 +33,63 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let difference = abs(currentValue - targetValue)
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) { action in
+            self.startNewRound()
+            self.updateLabel()
+        }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
 
     @IBAction func sliderMoved(_ sender: UISlider) {
-        print("The value of the slider is now: \(sender.value)")
+        currentValue = lroundf(sender.value)
+    }
+    
+    @IBAction func startOver(_ sender: Any) {
+        startNewGame()
+        updateLabel()
+    }
+    
+    func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+    }
+    
+    func updateLabel(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
